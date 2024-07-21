7c38bb358a4f65d6a8c97e17c142fa6b09885e26
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-21 15:52:24 +0300

    Create pessimistic_move_01.cpp

---
+++ pessimistic_move_01.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+struct A {
+	A()
+	{
+		std::cout << "default ctor\n";
+	}
+
+	A(const A&)
+	{
+		std::cout << "copy ctor\n";
+	}
+
+	A(A&&)
+	{
+		std::cout << "move  ctor\n";
+	}
+};
+
+A func()
+{
+	A ax;
+
+	return std::move(ax); //don't do this
+	//return ax; //do this
+}
+
+A foo()
+{
+	return std::move(A{}); //don't do this
+	//return A{}; //do this
+}
+
+A create()
+{
+	A ax;
+
+	//
+	return ax;
+}
+
+int main()
+{
+	A x{ foo() };
+	A y{ func() };
+	A z{ std::move(create())}; //don't do this
+}
