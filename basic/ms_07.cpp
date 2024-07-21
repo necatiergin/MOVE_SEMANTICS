df9b9f1414923cfab0a89cdac10398a439dacf6d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-21 16:06:32 +0300

    Update and rename const_return_value_01.cpp to basic/ms_07.cpp

---
+++ basic/ms_07.cpp
@@ -0,0 +1,25 @@
+#include <utility>
+#include <iostream>
+
+struct A {
+	A() = default;
+
+	//A(A&&)
+	A(A&&)noexcept
+	{
+		std::cout << "move ctor\n";
+	}
+
+	A(const A&)
+	{
+		std::cout << "copy ctor\n";
+	}
+};
+
+
+int main()
+{
+	A ax;
+
+	A bx(std::move_if_noexcept(ax));
+}
