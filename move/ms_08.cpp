bd092ac367fecf451e2a20e824ed7b67fc4b36e0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-25 13:22:46 +0300

    Update ms_08.cpp

--- move/ms_08.cpp
+++ move/ms_08.cpp
@@ -5,12 +5,12 @@
 
 struct StrWrapper
 {
-	// ensure each string has 100 characters:
-	StrWrapper() : m_str(1000, 'x') { }
-	
+	// ensure each string has 1000 characters:
+	StrWrapper() : m_str(100, 'x') { }
+
 	StrWrapper(const StrWrapper&) = default;
-	
-	StrWrapper(StrWrapper&& other): m_str{ std::move(other.m_str) } {}
+
+	StrWrapper(StrWrapper&& other)noexcept : m_str{ std::move(other.m_str) } {}
 private:
 	std::string m_str;
 };
@@ -19,7 +19,7 @@ int main()
 {
 	std::vector<StrWrapper> svec;
 	svec.resize(1'000'000u);
-	
+
 	auto t_start = std::chrono::steady_clock::now();
 	svec.reserve(svec.capacity() + 1);
 	auto t_end = std::chrono::steady_clock::now();
