490d4f82442e1710d9b5c5f229133c4f5f9d7b8b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-29 10:26:47 +0300

    Create ce_01.cpp

---
+++ copy_elision/ce_01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct A {
+	A()
+	{
+		std::cout << "default ctor this = " << this << '\n';
+	}
+
+	A(const A&) = delete;
+	A(A&&) = delete;
+};
+
+void foo(A) { }
+
+A bar()
+{
+	return A{};
+}
+
+int main()
+{
+	A ax = A{};  //mandatory copy elision
+	foo(A{});   //mandatory copy elision
+	A ay = bar();  //mandatory copy elision
+}
