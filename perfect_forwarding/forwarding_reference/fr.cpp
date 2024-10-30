3b71880b69f2a56d1aeb8d4681039f4c4f3c6139
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-30 09:39:56 +0300

    Update and rename explicit_arguments_01.cpp to fr.cpp

--- perfect_forwarding/forwarding_reference/fr.cpp
+++ perfect_forwarding/forwarding_reference/fr.cpp
@@ -1,18 +1,18 @@
+//explicit argument for universal reference params
+
 #include <string>
 
 template <typename T>
 void func(T&&);
 
-
-
 int main()
 {
-	using namespace std;
+	using std::string;
 
 	string name{ "necati" };
 
 	//func<string>(name); //error
-	func<string>(string{"ergin"}); //valid
+	func<string>(string{ "ergin" }); //valid
 	func<string&>(name); //valid
 	//func<string&>(string{ "ergin" }); //error
 	//func<string&&>(name); //error
