b35a19f09a6105fc56a7e238869bc3f1ec1cd4db
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-31 12:42:27 +0300

    Update fr_01.cpp

--- perfect_forwarding/forwarding_reference/fr_01.cpp
+++ perfect_forwarding/forwarding_reference/fr_01.cpp
@@ -3,13 +3,13 @@
 class Myclass {};
 
 using LRef = Myclass&;
-using LRefRef = Myclass&&;
+using RRef = Myclass&&;
 
 int main()
 {
 	Myclass m;
 	LRef& r1 = m;  //r1 is Myclass &
 	LRef&& r2 = m;  //r2 is Myclass &
-	LRefRef& r3 = m; // r3 is Myclass &
-	LRefRef&& r4 = Myclass{}; // r4 is Myclass &&
+	RRef& r3 = m; // r3 is Myclass &
+	RRef&& r4 = Myclass{}; // r4 is Myclass &&
 }
