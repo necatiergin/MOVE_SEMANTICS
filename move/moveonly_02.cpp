ac1d30c3b2f1f5df6c16c6eac2dfbced008cd694
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-05 10:24:17 +0300

    Create moveonly_02.cpp

---
+++ move/moveonly_02.cpp
@@ -0,0 +1,11 @@
+
+class MoveOnly {
+public:
+	MoveOnly();
+	MoveOnly(const MoveOnly&) = delete;
+	MoveOnly& operator=(const MoveOnly&) = delete;
+
+	MoveOnly(MoveOnly&&);  
+	MoveOnly& operator=(MoveOnly&&);    
+	//...	
+};
