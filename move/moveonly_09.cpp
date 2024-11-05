0e465fba796351dfbf0e5cac0a79d874776d1a88
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-05 10:48:44 +0300

    Create moveonly_09.cpp

---
+++ move/moveonly_09.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include <memory>
+
+using usptr = std::unique_ptr<std::string>;
+
+int main()
+{
+	usptr uptr = std::make_unique<std::string>("necati");
+
+	std::vector<usptr> vec(5, uptr); //gecersiz
+}
