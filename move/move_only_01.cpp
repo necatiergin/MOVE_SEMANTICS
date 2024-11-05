2d4774a0cb6e1c760fdb5bc6f81d4c61ba8e8bf2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-05 10:24:38 +0300

    Update move_only_01.cpp

--- move/move_only_01.cpp
+++ move/move_only_01.cpp
@@ -1,7 +1,7 @@
 class MoveOnly {
 public:
-	Moveonly();
-	Moveonly(MoveOnly&&);  //move ctor
+	MoveOnly();
+	MoveOnly(MoveOnly&&);  //move ctor
 	MoveOnly& operator=(MoveOnly&&);  //move assignment
 	// copy ctor deleted
 	// copy assignment deleted
