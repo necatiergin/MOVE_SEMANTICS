712676e7ea63de44247c0cb8fa806c79cbcbe6c7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-03-30 16:36:25 +0300

    Update ms_08.cpp

--- move/ms_08.cpp
+++ move/ms_08.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <thread>
 #include <chrono>
+
 
 struct StrWrapper
 {
 	// ensure each string has 1000 characters:
-	StrWrapper() : m_str(100, 'x') { }
+	StrWrapper() : m_str(1000, 'x') {}
 
 	StrWrapper(const StrWrapper&) = default;
 
-	StrWrapper(StrWrapper&& other)noexcept : m_str{ std::move(other.m_str) } {}
+	StrWrapper(StrWrapper&& other) : m_str{ std::move(other.m_str) } {}
 private:
 	std::string m_str;
 };
@@ -20,6 +22,7 @@ int main()
 	std::vector<StrWrapper> svec;
 	svec.resize(1'000'000u);
 
+	std::this_thread::sleep_for(std::chrono::milliseconds{ 500 });
 	auto t_start = std::chrono::steady_clock::now();
 	svec.reserve(svec.capacity() + 1);
 	auto t_end = std::chrono::steady_clock::now();
