b16014c1ddc2eed59d50f3c4ef13415f8ea9bd80
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-01 10:45:42 +0300

    Create ce_13.cpp

---
+++ copy_elision/ce_13.cpp
@@ -0,0 +1,29 @@
+// derleyicinin NRVO uygulayamadığı senaryolar
+
+#include <iostream>
+#include <cstdlib>
+
+struct Nec {
+    Nec() { std::cout << "default constructor\n"; }
+    Nec(const Nec&) { std::cout << "copy constructor\n"; }
+    Nec(Nec&&) { std::cout << "move constructor\n"; }
+    ~Nec() { std::cout << "destructor\n"; }
+};
+
+Nec foo(bool b) 
+{
+    Nec necx;
+    Nec necy;
+
+    if (b) {
+        return necx;
+    }
+    else {
+        return necy;
+    }
+}
+
+int main() 
+{
+    Nec s = foo(rand() % 2 == 0);
+} 
