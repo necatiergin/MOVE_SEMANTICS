0d96577d0c750e4419daa4e9f6bda10869b8f6bd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-01 13:09:52 +0300

    Create noexcept_02.cpp

---
+++ move/noexcept/noexcept_02.cpp
@@ -0,0 +1,17 @@
+#include <type_traits>
+
+template <typename T>
+void func(T) noexcept(std::is_nothrow_move_assignable_v<T>)
+{
+	//...
+}
+
+
+class Myclass {};
+
+int main()
+{
+	constexpr bool b1 = noexcept(func(12));
+	constexpr bool b2 = noexcept(func(Myclass{}));
+
+}
