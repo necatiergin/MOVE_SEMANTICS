b67d2e4f5869f43412c4c0aeedaf6a691669bccc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-04 15:44:37 +0300

    Update arr_01.cpp

--- perfect_forwarding/autorefref/arr_01.cpp
+++ perfect_forwarding/autorefref/arr_01.cpp
@@ -5,22 +5,19 @@
 template<typename Con, typename T>
 void Fill(Con& c, const T& value) {
 	for (auto& elem : c) {
-	//for (auto&& elem : c) {
+		//for (auto&& elem : c) {
 		elem = value;
 	}
 }
 
 int main()
 {
-	using namespace std;
-
-	vector<int> ivec(20);
-	vector<string> svec(20);
-	vector<bool> bvec(20);
+	std::vector<int> ivec(20);
+	std::vector<std::string> svec(20);
+	std::vector<bool> bvec(20);
 
 	Fill(ivec, 1);
 	Fill(svec, "necati");
 	Fill(bvec, true);
 }
-
 
